34b1cf76cce61919facadf671e5ea5573208a201
Kayra Bora Keskin <148430215+BugKillR@users.noreply.github.com>
2026-06-09 14:50:40 +0300

    Commit 6

--- MainProgram.cpp
+++ MainProgram.cpp
@@ -66,7 +66,7 @@ private:
 public:
     void add(const T& item) { items_.push_back(item); }
 
-    int size() const { items_.size(); }
+    int size() const {static_cast<int>( items_.size()); }
 
     T get(int index) const {
         if (index < 0 || index >= size())
